653eb5a6d103b8e0edd47a88293cac916ae40738
HR Williams <halrubinwilliams@gmail.com>
2021-01-30 17:09:00 -0800

    Change vars to lets, remove console.log

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
   $("#favoriteThings").submit(function(event) {
 
-    var food = $("#food").val();
-    var movie = $("#movie").val();
-    var animal = $("#animal").val();
-    var tvShow = $("#tvShow").val();
+    let food = $("#food").val();
+    let movie = $("#movie").val();
+    let animal = $("#animal").val();
+    let tvShow = $("#tvShow").val();
 
-    var allFav = [food, movie, animal, tvShow];
-    var allFavRevised = [];
+    let allFav = [food, movie, animal, tvShow];
+    let allFavRevised = [];
     allFavRevised.push(allFav[0], allFav[1], allFav[2], allFav[3]);
 
-    console.log(allFavRevised);
+    // console.log(allFavRevised);
     $("#resultOne").append("<li>" + allFavRevised[0] + "</li>");
     $("#resultOne").append("<li>" + allFavRevised[1] + "</li>");
     $("#resultOne").append("<li>" + allFavRevised[2] + "</li>");
